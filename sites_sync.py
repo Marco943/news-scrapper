1ec00aaf6bc8646678320b5b86a89d03dceebb3c
Marco <macto.14@gmail.com>
2024-01-27 14:43:56 -0300

    classe de noticias refatorado

--- sites_sync.py
+++ sites_sync.py
@@ -15,8 +15,12 @@ from icecream import ic
 class Site:
     site: str
     url: str
+    atualizar_noticias: callable
+    parser_noticias: callable
 
     def __post_init__(self):
+        self.atualizar_noticias = self.atualizar_noticias.__get__(self)
+        self.parser_noticias = self.parser_noticias.__get__(self)
         self._caminho = Path().joinpath("noticias").joinpath(f"{self.site}.json")
         self._caminho.parent.mkdir(exist_ok=True)
         self._ler_noticias()
@@ -42,178 +46,176 @@ class Site:
             self.noticias = pl.read_json(self._caminho)
         return self.noticias
 
-    def atualizar_noticias(self):
-        pass
+
+def parser_bbc(self: Site, noticia_tag: Tag) -> dict:
+    noticia = {"f": self.site}
+    noticia["mat"] = noticia_tag.text
+    noticia["url"] = noticia_tag.get("href")
+    noticia["img"] = noticia_tag.parent.parent.parent.find("img").get("src")
+    noticia["dt"] = pendulum.from_format(
+        noticia_tag.parent.parent.find("time").get("datetime"),
+        "YYYY-MM-DD",
+        tz="America/Sao_Paulo",
+    ).to_iso8601_string()
+    return noticia
 
 
-@dataclass
-class CnnEconomia(Site):
-    site: str = "cnn"
-    url: str = "https://www.cnnbrasil.com.br/economia/"
+def atualizar_bbc(self: Site):
+    s = httpx.Client()
+    s.headers.update({"User-Agent": UserAgent().random})
+    pag_inicial = self._construir_soup(s, self.url)
+    noticias = pag_inicial.find_all(
+        "a", href=re.compile(r"\.com\/portuguese\/articles\/.+")
+    )
+    # if noticia.get("href") not in self.noticias.get_column("url")
 
-    def parsear_noticia(self, s, noticia_url: str):
-        noticia = {"f": self.site, "url": noticia_url}
-        soup_materia = self._construir_soup(s, noticia_url)
+    noticias_atualizadas = [self.parser_noticias(x) for x in noticias]
+    noticias_atualizadas = filter(bool, noticias_atualizadas)
 
-        post_title = soup_materia.find("h1", class_="post__title")
-        if post_title is None:
-            return None
-        noticia["mat"] = post_title.text.strip()
-
-        picture = soup_materia.find("picture", class_="img__destaque")
-        if picture is not None:
-            noticia["img"] = picture.img.get("src")
-
-        data_post = soup_materia.find("span", class_="post__data").text.strip()
-        re_data = re.search(r"\d{2}\/\d{2}\/\d{4} às \d{1,2}:\d{2}$", data_post)
-        noticia["dt"] = pendulum.from_format(
-            re_data.group(0), "DD/MM/YYYY [às] HH:mm", tz="America/Sao_Paulo"
-        ).to_iso8601_string()
-
-        return noticia
-
-    def atualizar_noticias(self):
-        s = httpx.Client()
-        s.headers.update({"User-Agent": UserAgent().random})
-        pag_inicial = self._construir_soup(s, self.url)
-        noticias = pag_inicial.find_all(
-            "a", href=re.compile(r"\.com\.br\/economia\/.+")
-        )
-        noticias_url = [noticia.get("href") for noticia in noticias]
-
-        noticias_atualizadas = [self.parsear_noticia(s, x) for x in noticias_url]
-        noticias_atualizadas = filter(bool, noticias_atualizadas)
-
-        self._gravar_noticias(noticias_atualizadas)
+    self._gravar_noticias(noticias_atualizadas)
 
 
-@dataclass
-class BbcEconomia(Site):
-    site: str = "bbc"
-    url: str = "https://www.bbc.com/portuguese/topics/cvjp2jr0k9rt"
-
-    def parsear_noticia(self, noticia_tag: Tag):
-        noticia = {"f": self.site}
-        noticia["mat"] = noticia_tag.text
-        noticia["url"] = noticia_tag.get("href")
-        noticia["img"] = noticia_tag.parent.parent.parent.find("img").get("src")
-        noticia["dt"] = pendulum.from_format(
-            noticia_tag.parent.parent.find("time").get("datetime"),
-            "YYYY-MM-DD",
-            tz="America/Sao_Paulo",
-        ).to_iso8601_string()
-        return noticia
-
-    def atualizar_noticias(self):
-        s = httpx.Client()
-        s.headers.update({"User-Agent": UserAgent().random})
-        pag_inicial = self._construir_soup(s, self.url)
-        noticias = pag_inicial.find_all(
-            "a", href=re.compile(r"\.com\/portuguese\/articles\/.+")
-        )
-        # if noticia.get("href") not in self.noticias.get_column("url")
-
-        noticias_atualizadas = [self.parsear_noticia(x) for x in noticias]
-        noticias_atualizadas = filter(bool, noticias_atualizadas)
-
-        self._gravar_noticias(noticias_atualizadas)
+def parser_fsp(self: Site, noticia_tag: Tag) -> dict:
+    noticia = {"f": self.site}
+    noticia["mat"] = noticia_tag.find("title").text
+    noticia_url = re.findall(r"\*(http.*html)\"", noticia_tag.find("description").text)
+    if not noticia_url:
+        return None
+    noticia["url"] = noticia_url[0]
+    noticia_dt = re.findall(
+        r"(\d{2}\/\d{2}\/\d{4} - \d{2}h\d{2})\)\s?$",
+        noticia_tag.find("description").text,
+    )[0]
+    if not noticia_dt:
+        return None
+    noticia["dt"] = pendulum.from_format(
+        noticia_dt,
+        "MM/DD/YYYY [-] HH[h]mm",
+        tz="America/Sao_Paulo",
+    ).to_iso8601_string()
+    return noticia
 
 
-@dataclass
-class FolhaSPMercado(Site):
-    site: str = "folha-sp-mercado"
-    url: str = "https://feeds.folha.uol.com.br/mercado/rss091.xml"
+def atualizar_fsp(self: Site):
+    s = httpx.Client()
+    s.headers.update({"User-Agent": UserAgent().random})
+    pag_inicial = self._construir_soup(s, self.url)
+    noticias = pag_inicial.find_all("item")
+    noticias_atualizadas = [self.parser_noticias(x) for x in noticias]
+    noticias_atualizadas = filter(bool, noticias_atualizadas)
 
-    def parsear_noticia(self, noticia_tag: Tag):
-        noticia = {"f": self.site}
-        noticia["mat"] = noticia_tag.find("title").text
-        noticia_url = re.findall(
-            r"\*(http.*html)\"", noticia_tag.find("description").text
-        )
-        if not noticia_url:
-            return None
-        noticia["url"] = noticia_url[0]
-        noticia_dt = re.findall(
-            r"(\d{2}\/\d{2}\/\d{4} - \d{2}h\d{2})\)\s?$",
-            noticia_tag.find("description").text,
-        )[0]
-        if not noticia_dt:
-            return None
-        noticia["dt"] = pendulum.from_format(
-            noticia_dt,
-            "MM/DD/YYYY [-] HH[h]mm",
-            tz="America/Sao_Paulo",
-        ).to_iso8601_string()
-        return noticia
-
-    def atualizar_noticias(self):
-        s = httpx.Client()
-        s.headers.update({"User-Agent": UserAgent().random})
-        pag_inicial = self._construir_soup(s, self.url)
-        noticias = pag_inicial.find_all("item")
-        noticias_atualizadas = [self.parsear_noticia(x) for x in noticias]
-        noticias_atualizadas = filter(bool, noticias_atualizadas)
-
-        self._gravar_noticias(noticias_atualizadas)
+    self._gravar_noticias(noticias_atualizadas)
 
 
-@dataclass
-class ValorEcon(Site):
-    site: str = "valor-economico"
-    url: str = "https://pox.globo.com/rss/valor"
-
-    def parsear_noticia(self, noticia_tag: Tag):
-        noticia = {"f": self.site}
-        noticia["mat"] = noticia_tag.find("title").text.strip()
-        noticia["url"] = noticia_tag.find("guid").text
-        noticia["dt"] = pendulum.from_format(
-            noticia_tag.find("pubdate").text,
-            "ddd[,] DD MMM YYYY HH:mm:ss ZZ",
-            tz="America/Sao_Paulo",
-        ).to_iso8601_string()
-        noticia_media = noticia_tag.find("media:content")
-        noticia["img"] = noticia_media.get("url") if noticia_media is not None else None
-        return noticia
-
-    def atualizar_noticias(self):
-        s = httpx.Client()
-        s.headers.update({"User-Agent": UserAgent().random})
-        pag_inicial = self._construir_soup(s, self.url)
-        noticias = pag_inicial.find_all("item")
-        noticias_atualizadas = [self.parsear_noticia(x) for x in noticias]
-        noticias_atualizadas = filter(bool, noticias_atualizadas)
-
-        self._gravar_noticias(noticias_atualizadas)
+def parser_v_econ(self: Site, noticia_tag: Tag) -> dict:
+    noticia = {"f": self.site}
+    noticia["mat"] = noticia_tag.find("title").text.strip()
+    noticia["url"] = noticia_tag.find("guid").text
+    noticia["dt"] = pendulum.from_format(
+        noticia_tag.find("pubdate").text,
+        "ddd[,] DD MMM YYYY HH:mm:ss ZZ",
+        tz="America/Sao_Paulo",
+    ).to_iso8601_string()
+    noticia_media = noticia_tag.find("media:content")
+    noticia["img"] = noticia_media.get("url") if noticia_media is not None else None
+    return noticia
 
 
-@dataclass
-class G1Econ(Site):
-    site: str = "g1-economia"
-    url: str = "https://g1.globo.com/rss/g1/economia/"
+def atualizar_v_econ(self: Site):
+    s = httpx.Client()
+    s.headers.update({"User-Agent": UserAgent().random})
+    pag_inicial = self._construir_soup(s, self.url)
+    noticias = pag_inicial.find_all("item")
+    noticias_atualizadas = [self.parser_noticias(x) for x in noticias]
+    noticias_atualizadas = filter(bool, noticias_atualizadas)
 
-    def parsear_noticia(self, noticia_tag: Tag):
-        noticia = {"f": self.site}
-        noticia["mat"] = noticia_tag.find("title").text.strip()
-        noticia["url"] = noticia_tag.find("guid").text
-        noticia["dt"] = pendulum.from_format(
-            noticia_tag.find("pubdate").text,
-            "ddd[,] DD MMM YYYY HH:mm:ss ZZ",
-            tz="America/Sao_Paulo",
-        ).to_iso8601_string()
-        noticia_media = noticia_tag.find("media:content")
-        noticia["img"] = noticia_media.get("url") if noticia_media is not None else None
-        return noticia
-
-    def atualizar_noticias(self):
-        s = httpx.Client()
-        s.headers.update({"User-Agent": UserAgent().random})
-        pag_inicial = self._construir_soup(s, self.url)
-        noticias = pag_inicial.find_all("item")
-        noticias_atualizadas = [self.parsear_noticia(x) for x in noticias]
-        noticias_atualizadas = filter(bool, noticias_atualizadas)
-
-        self._gravar_noticias(noticias_atualizadas)
+    self._gravar_noticias(noticias_atualizadas)
 
 
-self = G1Econ()
-self.atualizar_noticias()
+def parser_g1_econ(self: Site, noticia_tag: Tag) -> dict:
+    noticia = {"f": self.site}
+    noticia["mat"] = noticia_tag.find("title").text.strip()
+    noticia["url"] = noticia_tag.find("guid").text
+    noticia["dt"] = pendulum.from_format(
+        noticia_tag.find("pubdate").text,
+        "ddd[,] DD MMM YYYY HH:mm:ss ZZ",
+        tz="America/Sao_Paulo",
+    ).to_iso8601_string()
+    noticia_media = noticia_tag.find("media:content")
+    noticia["img"] = noticia_media.get("url") if noticia_media is not None else None
+    return noticia
+
+
+def atualizar_g1_econ(self: Site):
+    s = httpx.Client()
+    s.headers.update({"User-Agent": UserAgent().random})
+    pag_inicial = self._construir_soup(s, self.url)
+    noticias = pag_inicial.find_all("item")
+    noticias_atualizadas = [self.parser_noticias(x) for x in noticias]
+    noticias_atualizadas = filter(bool, noticias_atualizadas)
+
+    self._gravar_noticias(noticias_atualizadas)
+
+
+def parser_cnn_econ(self: Site, s: httpx.Client, noticia_url: str) -> dict:
+    noticia = {"f": self.site, "url": noticia_url}
+    soup_materia = self._construir_soup(s, noticia_url)
+
+    post_title = soup_materia.find("h1", class_="post__title")
+    if post_title is None:
+        return None
+    noticia["mat"] = post_title.text.strip()
+
+    picture = soup_materia.find("picture", class_="img__destaque")
+    if picture is not None:
+        noticia["img"] = picture.img.get("src")
+
+    data_post = soup_materia.find("span", class_="post__data").text.strip()
+    re_data = re.search(r"\d{2}\/\d{2}\/\d{4} às \d{1,2}:\d{2}$", data_post)
+    noticia["dt"] = pendulum.from_format(
+        re_data.group(0), "DD/MM/YYYY [às] HH:mm", tz="America/Sao_Paulo"
+    ).to_iso8601_string()
+
+    return noticia
+
+
+def atualizar_cnn_econ(self: Site):
+    s = httpx.Client()
+    s.headers.update({"User-Agent": UserAgent().random})
+    pag_inicial = self._construir_soup(s, self.url)
+    noticias = pag_inicial.find_all("a", href=re.compile(r"\.com\.br\/economia\/.+"))
+    noticias_url = [noticia.get("href") for noticia in noticias]
+
+    noticias_atualizadas = [self.parser_noticias(s, x) for x in noticias_url]
+    noticias_atualizadas = filter(bool, noticias_atualizadas)
+
+    self._gravar_noticias(noticias_atualizadas)
+
+
+bbc = Site(
+    "bbc",
+    "https://www.bbc.com/portuguese/topics/cvjp2jr0k9rt",
+    atualizar_bbc,
+    parser_bbc,
+)
+fsp = Site(
+    "folha-sp-mercado",
+    "https://feeds.folha.uol.com.br/mercado/rss091.xml",
+    atualizar_fsp,
+    parser_fsp,
+)
+v_econ = Site(
+    "valor-economico",
+    "https://pox.globo.com/rss/valor",
+    atualizar_v_econ,
+    parser_v_econ,
+)
+g1econ = Site(
+    "g1-economia",
+    "https://g1.globo.com/rss/g1/economia/",
+    atualizar_g1_econ,
+    parser_g1_econ,
+)
+cnn = Site(
+    "cnn", "https://www.cnnbrasil.com.br/economia/", atualizar_cnn_econ, parser_cnn_econ
+)
